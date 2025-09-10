efc8626673a3af88cf8594b27f2273df8d3a2989
nicodoido <nickdavid1407@gmail.com>
2025-09-10 09:31:28 -0300

    fix: cards e button removidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,28 +16,7 @@ export default function Home() {
     <div className="flex flex-col items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       
       
-      <XploraHero />
-
-      
-      <Button className="mt-10">Botão teste do Shadcnui</Button>
-
-      <div className="justify-between sm:mt-px100">
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>Card Exemplo Shadcnui</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <App />
-      </div>
-
-      
+      <XploraHero/>
     </div>
   );
 }
